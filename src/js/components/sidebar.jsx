35e1e7f745cdc6febd7879770831b49201c2552b
Kevin Østerkilde <kevin@oesterkilde.dk>
2016-11-19 17:28:24 +0100

    Uncommented publicPath

--- src/js/components/sidebar.jsx
+++ src/js/components/sidebar.jsx
@@ -18,6 +18,14 @@ class Sidebar extends React.Component {
         <div className="chamber-triple--top">
           This is a React app. There&apos;s no database at this point, all the data is gathered from a .json file instead.
         </div>
+        <div className="push-triple--top">
+          <h1>Header 1</h1>
+          <h2>Header 2</h2>
+          <h3>Header 3</h3>
+          <h4>Header 4</h4>
+          <h5>Header 5</h5>
+          <h6>Header 6</h6>
+        </div>
         <footer className="text--center">
           <span>
             Copyright &copy; 2016 Copyright Holder All Rights Reserved.
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: PATHS.build,
-    // publicPath: '/ReactJS-exercis/', // Uncomment this when deploying to GH-Pages
+    publicPath: '/ReactJS-exercis/', // Uncomment this when deploying to GH-Pages
     filename: '[name].js',
   },
   devtool: debug ? 'inline-sourcemap' : null,
